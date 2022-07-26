9b59beb7a75a3a854f8de9e105d3b93751965651
Igor Libukin <ulterpwnz@list.ru>
2022-07-26 23:51:01 +0300

    10. Installing and setting up Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
 require('./bootstrap');
+
+import router from "./routes"
+import VueRouter from "vue-router"
 
 window.Vue = require('vue').default;
 
@@ -26,7 +24,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+Vue.use(VueRouter)
 const app = new Vue({
     el: '#app',
+    router
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LaravelBnb</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>LaravelBnb</title>
 
-        <!-- Styles -->
-     
-<script src="{{ asset('js/app.js') }}" defer></script>
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="app"></div>
-    </body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</body>
+
 </html>
