8a30e5868537f09498728b3b48e47a4be646c24a
Igor Libukin <ulterpwnz@list.ru>
2022-07-27 00:05:51 +0300

    11. HTML5 history mode in Vue Router

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,15 +1,21 @@
 import VueRouter from "vue-router"
-import ExampleComponent from "./components/ExampleComponent.vue"
-
+import ExampleComponent from "./components/ExampleComponent"
+import Example2 from "./components/Example2"
 const routes = [
 	{
 		path: "/",
 		component: ExampleComponent,
 		name:'home',
 	},
+	{
+		path: "/second/",
+		component: Example2,
+		name:'second',
+	},
 ]
 
 const router = new VueRouter({
+	mode:"history",
 	routes: routes,
 })
 
