4384a781cf644f52c82888d7b6c034bce1bb7db9
Igor Libukin <ulterpwnz@list.ru>
2022-07-28 20:59:10 +0300

    13. Router Link component and routing inside a Vue application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,16 @@
+require("./bootstrap");
 
-require('./bootstrap');
+import VueRouter from "vue-router";
+import Index from "./Index";
+import router from "./routes";
 
-import router from "./routes"
-import VueRouter from "vue-router"
+window.Vue = require("vue/dist/vue.js");
+Vue.use(VueRouter);
 
-window.Vue = require('vue').default;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-Vue.use(VueRouter)
 const app = new Vue({
-    el: '#app',
-    router
+    el: "#app",
+    router,
+    components: {
+        index: Index,
+    },
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 
 <body>
     <div id="app">
-        <router-view></router-view>
+        <index></index>
     </div>
 </body>
 
