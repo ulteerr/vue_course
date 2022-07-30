def1ef277b474f6c94a94fd774f81ce4ba124aba
Igor Libukin <ulterpwnz@list.ru>
2022-07-30 15:46:00 +0300

    27. Component methods

--- public/js/app.js
+++ public/js/app.js
@@ -5305,6 +5305,14 @@ __webpack_require__.r(__webpack_exports__);
       return this.bookables === null ? 0 : Math.ceil(this.bookables.length / this.columns);
     }
   },
+  methods: {
+    bookablesInRow: function bookablesInRow(row) {
+      return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+    },
+    placeholdersInRow: function placeholdersInRow(row) {
+      return this.columns - this.bookablesInRow(row).length;
+    }
+  },
   created: function created() {
     var _this = this;
 
@@ -5473,16 +5481,28 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div", [_vm._v("\n  Rows is: " + _vm._s(_vm.rows) + "\n  "), _vm.loading ? _c("div", [_vm._v("Data is loading...")]) : _c("div", _vm._l(_vm.bookables, function (bookable, index) {
-    return _c("bookable-list-item", {
-      key: index,
-      attrs: {
-        "item-title": bookable.title,
-        "item-content": bookable.content,
-        price: 1000
-      }
-    });
-  }), 1)]);
+  return _c("div", [_vm.loading ? _c("div", [_vm._v("Data is loading...")]) : _c("div", _vm._l(_vm.rows, function (row) {
+    return _c("div", {
+      key: "row" + row,
+      staticClass: "row mb-4"
+    }, [_vm._l(_vm.bookablesInRow(row), function (bookable, column) {
+      return _c("div", {
+        key: "row" + row + column,
+        staticClass: "col"
+      }, [_c("bookable-list-item", {
+        attrs: {
+          "item-title": bookable.title,
+          "item-content": bookable.content,
+          price: 1000
+        }
+      })], 1);
+    }), _vm._v(" "), _vm._l(_vm.placeholdersInRow(row), function (p) {
+      return _c("div", {
+        key: "placeholder" + row + p,
+        staticClass: "col"
+      });
+    })], 2);
+  }), 0)]);
 };
 
 var staticRenderFns = [];
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    Rows is: {{ rows }}
     <div v-if="loading">Data is loading...</div>
     <div v-else>
-      <bookable-list-item
-        :item-title="bookable.title"
-        :item-content="bookable.content"
-        :price="1000"
-        v-for="(bookable, index) in bookables"
-        :key="index"
-      ></bookable-list-item>
+      <div class="row mb-4" v-for="row in rows" :key="'row' + row">
+        <div
+          class="col"
+          v-for="(bookable, column) in bookablesInRow(row)"
+          :key="'row' + row + column"
+        >
+          <bookable-list-item
+            :item-title="bookable.title"
+            :item-content="bookable.content"
+            :price="1000"
+          ></bookable-list-item>
+        </div>
+
+        <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,20 +26,28 @@ import BookableListItem from "./BookableListItem";
 
 export default {
   components: {
-    BookableListItem,
+    BookableListItem
   },
   data() {
     return {
       bookables: null,
       loading: false,
-      columns:3,
+      columns: 3
     };
   },
-  computed:{
-    rows(){
+  computed: {
+    rows() {
       return this.bookables === null
-      ? 0 
-      : Math.ceil(this.bookables.length / this.columns)
+        ? 0
+        : Math.ceil(this.bookables.length / this.columns);
+    }
+  },
+  methods: {
+    bookablesInRow(row) {
+      return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+    },
+    placeholdersInRow(row) {
+      return this.columns - this.bookablesInRow(row).length;
     }
   },
   created() {
@@ -42,39 +57,40 @@ export default {
         {
           id: 1,
           title: "Cheap Villa !!!",
-          content: "A very cheap villa",
+          content: "A very cheap villa"
         },
         {
           title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
+          content: "A very cheap villa 2"
         },
         {
           title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
+          content: "A very cheap villa 2"
         },
         {
           title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
-        },
-          {
-          title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
+          content: "A very cheap villa 2"
         },
         {
           title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
-        },
-          {
-          title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
+          content: "A very cheap villa 2"
         },
         {
           title: "Cheap Villa 2",
-          content: "A very cheap villa 2",
+          content: "A very cheap villa 2"
         },
+        {
+          title: "Cheap Villa 2",
+          content: "A very cheap villa 2"
+        }
+        ,
+        {
+          title: "Cheap Villa 2",
+          content: "A very cheap villa 2"
+        }
       ];
       this.loading = false;
     }, 2000);
-  },
+  }
 };
 </script>
