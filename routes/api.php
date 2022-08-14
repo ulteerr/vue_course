086c839c58715340cfcc42edd2104dba2e1e8820
Igor Libukin <ulterpwnz@list.ru>
2022-08-14 13:15:34 +0300

    43. Laravel controllers

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
-use App\Models\Bookable;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bookables', function (Request $request) {
-    return Bookable::all();
-});
-
-Route::get('bookables/{id}', function (Request $request, $id) {
-    return Bookable::findOrFail($id);
-});
+Route::get('bookables', [\App\Http\Controllers\Api\BookableController::class, 'index']);
+Route::get('bookables/{id}', [\App\Http\Controllers\Api\BookableController::class, 'show']);
