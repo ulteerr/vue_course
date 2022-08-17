e5694a7007fe76be830c2434de5b5ae471f2f6db
Igor Libukin <ulterpwnz@list.ru>
2022-08-17 23:14:28 +0300

    #68

--- public/js/app.js
+++ public/js/app.js
@@ -5531,15 +5531,29 @@ __webpack_require__.r(__webpack_exports__);
   },
   computed: {
     halfStar: function halfStar() {
-      return true;
+      var fraction = Math.round((this.rating - Math.floor(this.rating)) * 100); // console.log(fraction);
+
+      return fraction > 0 && fraction < 50;
     },
     fullStars: function fullStars() {
-      return 3;
+      // > 4.5 = 5 stars
+      // 4.3 = 4 and half
+      return Math.round(this.rating);
     },
     emptyStars: function emptyStars() {
-      return 1;
+      // if rating would be 1.9, ceil(1.9) = 2, 5 - 2 = 3, render 3 empty stars
+      return 5 - Math.ceil(this.rating);
     }
-  }
+  } // created() {
+  //   const numbers = [0.9, 4.0, 4.4, 4.5, 4.6, 4.9];
+  //   numbers.forEach(n => {
+  //     console.log(`round for ${n} is ${Math.round(n)}`);
+  //     console.log(`floor for ${n} is ${Math.floor(n)}`);
+  //     console.log(`ceil for ${n} is ${Math.ceil(n)}`);
+  //     console.log("==================================");
+  //   });
+  // }
+
 });
 
 /***/ }),
@@ -5783,7 +5797,12 @@ var render = function render() {
       staticClass: "col-md-6"
     }, [_vm._v("Piotr Jura")]), _vm._v(" "), _c("div", {
       staticClass: "col-md-6 d-flex justify-content-end"
-    }, [_vm._v("\n          " + _vm._s(review.rating) + "\n          "), _c("star-rating")], 1)]), _vm._v(" "), _c("div", {
+    }, [_c("star-rating", {
+      staticClass: "fa-lg",
+      attrs: {
+        rating: review.rating
+      }
+    })], 1)]), _vm._v(" "), _c("div", {
       staticClass: "row"
     }, [_c("div", {
       staticClass: "col-md-12"
