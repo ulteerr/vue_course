705efb429020ed896617c23bac147cd470be0011
igorlibukin <ulterpwnz@list.ru>
2022-08-19 16:35:42 +0300

    fatal error file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,24 @@
 require("./bootstrap");
 
-import Vue from "vue";
 import moment from "moment";
 import VueRouter from "vue-router";
 import Index from "./Index";
 import router from "./routes";
+import FatalError from "./shared/components/FatalError";
 import StarRating from "./shared/components/StarRating";
 
-window.Vue = require("vue/dist/vue.js");
+window.Vue = require("vue");
 Vue.use(VueRouter);
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 
 Vue.component("star-rating", StarRating);
+Vue.component("fatal-error", FatalError);
 
 const app = new Vue({
     el: "#app",
     router,
     components: {
-        index: Index,
-    },
+        index: Index
+    }
 });
