8f3d384dfb2de5a77ddd6362256160f29b96e617
igorlibukin <ulterpwnz@list.ru>
2022-08-19 17:45:11 +0300

    setting uo vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,24 @@
 require("./bootstrap");
 
 import Vue from "vue";
-import moment from "moment";
 import VueRouter from "vue-router";
+import Vuex from 'vuex'
+import moment from "moment";
+
 import Index from "./Index";
 import router from "./routes";
 import FatalError from "./shared/components/FatalError";
 import StarRating from "./shared/components/StarRating";
 import Success from "./shared/components/Success";
 import ValidationErrors from "./shared/components/ValidationErrors";
+import storeDefinition from "./store";
+
 
 window.Vue = require("vue/dist/vue.js");
+
 Vue.use(VueRouter);
+Vue.use(Vuex)
+
 
 Vue.filter("fromNow", value => moment(value).fromNow());
 
@@ -20,10 +27,15 @@ Vue.component("fatal-error", FatalError);
 Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors);
 
+const store = new Vuex.Store(storeDefinition);
+
+
 const app = new Vue({
     el: "#app",
     router,
+    store,
     components: {
         index: Index
     }
 });
+
